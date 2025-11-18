57051b22dfdca5622585bcb65f13c802c215dc7a
WannaBeCoder <wannabecoder11@gmail.com>
2025-11-18 16:59:16 +0530

    added functions for getting distinct resources

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,55 @@ const { parse } = require('path');
 const url = require('url');
 const { createClient } = require('@clickhouse/client');
 
+const client = createClient({
+        url: process.env.CLICKHOUSE_HOST ?? 'http://localhost:8123',
+        username: process.env.CLICKHOUSE_USER ?? 'default',
+        password: process.env.CLICKHOUSE_PASSWORD ?? '', }); // adjust config
+let resultJSON 
+async function run() {
+  try {
+    const resultSet = await client.query({
+      query: `SELECT * FROM signoz_logs.logs_v2
+              WHERE
+                  resource_fingerprint IN (
+                      SELECT fingerprint 
+                      FROM signoz_logs.logs_v2_resource 
+                      WHERE simpleJSONExtractString(labels, 'k8s.cluster.name') = 'test-env'
+                  )
+              AND timestamp >= 1763014613000000000
+              AND timestamp <= 1763015111000000000
+              ORDER BY timestamp ASC
+              LIMIT 10`,
+      format: 'JSONEachRow',
+    });
+
+    const stream = resultSet.stream();
+     // fetch all results as JSON array
+    const allRows = await resultSet.json();
+    resultJSON = allRows;
+
+    // console.log(allRows); // allRows is an array of row objects
+    stream.on('data', (rows) => {
+      rows.forEach(row => {
+        // console.log(row); // 'row' is already parsed JSON object
+      });
+    });
+
+    stream.on('end', () => {
+      console.log('Completed!');
+    });
+
+    stream.on('error', (err) => {
+      console.error('Stream error:', err);
+    });
+  } catch (err) {
+    console.error('Query error:', err);
+  }
+}
+
+
+
+run().catch(console.error);
 
 
 const server = http.createServer((req, res) => {
@@ -23,7 +72,7 @@ const server = http.createServer((req, res) => {
        res.end(JSON.stringify({
         pathname,
         query,
-        fullUrl: req.url
+        fullUrl: resultJSON
             }, null, 2));
 //    }
    
@@ -31,48 +80,38 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000);
 console.log('Listening on Port 3000');
+let distinctEnv
+let distinctHost
+let distinctDeploy
+let distinctDaemon
+let distinctCluster
 
-const client = createClient({
-        url: process.env.CLICKHOUSE_HOST ?? 'http://localhost:8123',
-        username: process.env.CLICKHOUSE_USER ?? 'default',
-        password: process.env.CLICKHOUSE_PASSWORD ?? '', }); // adjust config
-
-async function run() {
+async function getDistinctResources() {
   try {
     const resultSet = await client.query({
-      query: `SELECT * FROM signoz_logs.logs_v2
-              WHERE
-                  resource_fingerprint IN (
-                      SELECT fingerprint 
-                      FROM signoz_logs.logs_v2_resource 
-                      WHERE simpleJSONExtractString(labels, 'k8s.cluster.name') = 'test-env'
-                  )
-              AND timestamp >= 1763014613000000000
-              AND timestamp <= 1763015111000000000
-              ORDER BY timestamp ASC
-              LIMIT 10`,
+      query: `SELECT DISTINCT labels
+      FROM signoz_logs.logs_v2_resource
+      Limit 1000;`,
+
       format: 'JSONEachRow',
     });
+     // fetch all results as JSON array
+    const allRows = await resultSet.json();
+    // console.log(allRows)
+    distinctEnv = [...new Set(allRows.map(obj => JSON.parse(obj.labels)["deployment.environment"]))];
+    distinctHost = [...new Set(allRows.map(obj => JSON.parse(obj.labels)["host.name"]))];
+    distinctDeploy = [...new Set(allRows.map(obj => JSON.parse(obj.labels)["k8s.deployment.name"]))];
+    distinctDaemon = [...new Set(allRows.map(obj => JSON.parse(obj.labels)["k8s.daemonset.name"]))];
+    distinctCluster = [...new Set(allRows.map(obj => JSON.parse(obj.labels)["k8s.cluster.name"]))];
 
-    const stream = resultSet.stream();
-    stream.on('data', (rows) => {
-      rows.forEach(row => {
-        console.log(row); // 'row' is already parsed JSON object
-      });
-    });
+    console.log(`distinct envs are ${distinctEnv}`); // allRows is an array of row objects
+    console.log(`distinct Hosts are ${distinctHost}`); // allRows is an array of row objects
+    console.log(`distinct Deployments are ${distinctDeploy}`); // allRows is an array of row objects
+    console.log(`distinct Daemonsets are ${distinctDaemon}`); // allRows is an array of row objects
+    console.log(`distinct Clusters are ${distinctCluster}`); // allRows is an array of row objects
 
-    stream.on('end', () => {
-      console.log('Completed!');
-    });
-
-    stream.on('error', (err) => {
-      console.error('Stream error:', err);
-    });
   } catch (err) {
     console.error('Query error:', err);
   }
 }
-
-
-
-run().catch(console.error);
+getDistinctResources().catch(console.error);
