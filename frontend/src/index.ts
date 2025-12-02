3da6fdb99c6de32acbbd8999fb70174e40c5737a
WannaBeCoder <wannabecoder11@gmail.com>
2025-12-02 15:20:52 +0530

    cleaned older code from FE JS, removed displayValues function, fixed but for hosts hiddev div in html

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,52 +1,3 @@
-let getDistinctResources 
-
-fetch('http://localhost:3000/api/distinct')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-    //   console.table(response);
-    getDistinctResources = response
-
-    // displayValues(getDistinctResources["distinctEnv"], "environment");
-    // displayValues(getDistinctResources["distinctHost"], "host")
-    // displayValues(getDistinctResources["distinctCluster"], "cluster")
-    // displayValues(getDistinctResources["distinctDeploy"], "deploy")
-    // displayValues(getDistinctResources["distinctDaemon"], "daemon")
-
-    });;
-
-function displayValues(distinctValues: string[], resourceType: string) {
-      document.getElementById(`${resourceType}Keys`)?.toggleAttribute("hidden")
-
-      const envDiv = document.getElementById(`${resourceType}`);
-      if (envDiv) {
-      envDiv.replaceChildren();
-      }
-  distinctValues.forEach(item => {
-      // console.log(item);
-      const envListDiv = document.createElement("div")
-      const envListInput = document.createElement("input")
-      const envListLabel = document.createElement("label")
-      envListDiv.setAttribute("class", "option")
-      envListInput.setAttribute("type", "checkbox" )
-      envListInput.setAttribute("id", item);
-      envListInput.setAttribute("name", resourceType)
-      envListInput.setAttribute("value", item)
-      envListLabel.setAttribute("for", item)
-      if (envDiv) {
-
-        envDiv.appendChild(envListDiv)
-
-        envDiv.removeAttribute("hidden")
-        envListLabel.innerText = item;
-        envListDiv.appendChild(envListInput)
-        envListDiv.appendChild(envListLabel)
-      }
-    })}
-
-
-
 function displayClusters(distinctValues: string[], resourceType: string) {
   document.getElementById(`${resourceType}Keys`)?.toggleAttribute("hidden");
   document.getElementById(`deploymentsKeys`)?.toggleAttribute("hidden"); // show deployment heading
@@ -83,51 +34,6 @@ function displayClusters(distinctValues: string[], resourceType: string) {
         envListDiv.appendChild(envListLabel)
       }
     })}
-
-const k8sDiv = document.getElementById(`k8sType`);
-k8sDiv?.addEventListener("click", (e) => {
-
-  fetch('http://localhost:3000/api/clusters')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-      const clusters: Array<string> = response
-      // console.log("The list of clusters are " + clusters)
-      displayClusters(clusters, "clusters");
-})
-
-})
-
-
-
-const nonK8sDiv = document.getElementById(`nonK8sType`);
-nonK8sDiv?.addEventListener("click", (e) => {
-
-  fetch('http://localhost:3000/api/hosts')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-      const hosts: Array<string> = response
-      displayValues(hosts, "host");
-})
-})
-
-// const envListDiv = document.createElement("div")
-// const envListInput = document.createElement("input")
-// const envListLabel = document.createElement("label")
-// envListDiv.setAttribute("class", "option")
-// envListInput.setAttribute("type", "checkbox" )
-// envListInput.setAttribute("id", distinctValues[key]);
-// envListInput.setAttribute("name", e)
-// envListInput.setAttribute("value", distinctValues[key])
-// envListLabel.setAttribute("for", distinctValues[key])
-// envListLabel.innerText = distinctValues[key];
-// envDiv.appendChild(envListDiv)
-// envListDiv.appendChild(envListInput)
-// envListDiv.appendChild(envListLabel)
-// envListDiv.onclick 
 
 function displayList(distinctValues: string[], resourceType: string) {
       // document.getElementById(`${resourceType}Keys`)?.removeAttribute("hidden")
@@ -186,3 +92,33 @@ function fetchK8sResources(resourceType: string) {
 
 
       }
+const k8sDiv = document.getElementById(`k8sType`);
+k8sDiv?.addEventListener("click", (e) => {
+
+  fetch('http://localhost:3000/api/clusters')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+      const clusters: Array<string> = response
+      // console.log("The list of clusters are " + clusters)
+      displayClusters(clusters, "clusters");
+})
+
+})
+
+
+
+const nonK8sDiv = document.getElementById(`nonK8sType`);
+nonK8sDiv?.addEventListener("click", (e) => {
+  fetch('http://localhost:3000/api/hosts')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+      const hosts: Array<string> = response
+      document.getElementById(`hostKeys`)?.toggleAttribute("hidden");
+      displayList(hosts, "host");
+})
+})
+
